86522e1d5911952440450e8ed711c6f75c93de4b
Moril jean <MorilFrancois.Amaris@esmax.cl>
2025-05-16 20:34:07 -0400

    first commit

--- maps_app/ios/Runner/AppDelegate.swift
+++ maps_app/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyAMtW6IAyJIkO6DVIK8uAXfBCXyEdF0kSM")
+    GMSServices.provideAPIKey("")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
